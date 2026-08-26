219a3e1a5b4c52566d7c801b6890e5f08f7edc19
vishnu-prasath-M <imvishnuprasath@gmail.com>
2026-08-26 15:31:03 +0530

    Remove enum restriction on Withdrawal.withdrawType to allow any plan type string

--- server/models/Withdrawal.js
+++ server/models/Withdrawal.js
@@ -30,7 +30,6 @@ const withdrawalSchema = new mongoose.Schema({
   },
   withdrawType: {
     type: String,
-    enum: ['saving', 'fixed', '15_days', '1_month', '3_months', '6_months', '1_year', '2_years', 'investment'],
     default: 'saving',
   },
   investmentId: {
